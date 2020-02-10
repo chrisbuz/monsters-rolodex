9abcccf575e8b033bfc8e42e56aac54d3116b527
Christian Buzombo <360@gmail.com>
2020-02-09 23:26:21 -0600

    Updates

---
+++ precache-manifest.a70ff6ec2ffe4d54665bbd81591d28c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d21b0501a4403f790f5e7cf1bb01850",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8bbad2b4ce857758575c",
+    "url": "/monsters-rolodex/static/css/main.24bb8a9f.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bbad2b4ce857758575c",
+    "url": "/monsters-rolodex/static/js/main.e122182f.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
